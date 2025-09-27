6bfb5808282e3992bb2d6aefe8fde11ebf4c730f
Maurice Robert <mcosterobert@gmail.com>
2025-09-26 22:29:27 -0500

    Update metro config to ensurce console logs are removed from minified production code. From official minify guide: https://docs.expo.dev/guides/minify/#remove-console-logs

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,10 @@ const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 defaultConfig.resolver.unstable_enablePackageExports = false; // Add this line
 
+defaultConfig.transformer.minifierConfig = {
+  compress: {
+    drop_console: true,
+  }
+};
+
 module.exports = defaultConfig;
